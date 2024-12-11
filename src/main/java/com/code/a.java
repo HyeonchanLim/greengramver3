4e03a9aa1c3efb1c49a1fdd70a45fe8a3be1a673
LimHyeonChan <lhc0240@gmail.com>
2024-12-11 19:04:39 +0900

    aa

--- src/main/java/com/code/a.java
+++ src/main/java/com/code/a.java
@@ -1,9 +1,8 @@
 package com.code;
 
 public class a {
-    public int solution(int slice, int n , String ss) {
-        // 필요한 최소 판 수 계산
-        return (int)Math.ceil((double)n/slice);
-        
+    public static void main(String[] args) {
+        String aa = "qwerasdfqwer";
+
     }
 }
