d2ae1c757dab9d3fdaefd5ab1526c34b996e3434
LimHyeonChan <lhc0240@gmail.com>
2024-12-16 09:36:34 +0900

    aa

--- src/main/java/com/code/a.java
+++ src/main/java/com/code/a.java
@@ -1,18 +1,5 @@
 package com.code;
 
 public class a {
-    public int[] solution(int[] numbers, String direction) {
-        int[] answer = {};
-        int max = numbers.length-1;
 
-        if (direction.equals("right")){
-            answer[0] = numbers[max];
-            for (int i=1; i<max; i++){
-                answer[i+1] = answer[i];
-            }
-        }
-        } else if (direction.equals("right")) {
-
-        return answer;
-    }
 }
